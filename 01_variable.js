a15eeb5f43bfbe5dcc05ab425603885cd2afd02c
Axjer <axjer96@gmail.com>
2026-07-16 15:53:31 +0800

    add topic 01 and 02

---
+++ 01_variable.js
@@ -0,0 +1,16 @@
+// 變數：用來儲存值的容器
+// 宣告：let x;
+// 賦值：x = 100;
+
+let x;
+x = 100;
+console.log(x); //100
+
+let y = 200;
+console.log(y); //200
+
+let name = "Qiu";
+console.log(`Your name is ${name}`); //Your name is Qiu
+
+let isStudent = true;
+console.log(`Enrolled: ${isStudent}`); //Enrolled: true
---
+++ 02_declaration.js
@@ -0,0 +1,38 @@
+// var vs let vs const
+
+// var 可重複宣告、可重新賦值、有提升(Hoisting)、作用域：函式(Function Scope)
+
+var x = 1;
+var x = 2;
+console.log(x); //2
+
+console.log(hello);
+var hello = "say hello"; //宣告變數，但未初始化值，輸出：undefined
+
+// let 不可重複宣告、可重新賦值、無提升、作用域：區塊(Block Scope)
+
+let y = 3;
+y = 4;
+console.log(y); //4
+
+/*
+若執行這段程式碼，此時會出現SyntaxError
+let y = 4;
+console.log(y);
+*/
+
+// const 為常數不可重複宣告、不可重新賦值、無提升、作用域：區塊(Block Scope)
+
+const z = 5;
+console.log(z); //5
+/*
+若執行這段程式碼，此時會出現SyntaxError
+const z = 6;
+console.log(z);
+*/
+
+/*
+若執行這段程式碼，此時會出現TypeError
+z = 6;
+console.log(z);
+*/
