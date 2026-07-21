1be28de75b776422e8aab9d1854947c17c881504
Axjer <axjer96@gmail.com>
2026-07-21 19:46:05 +0800

    Update 04_user input.js

--- 04_user input.js
+++ 04_user input.js
@@ -2,6 +2,6 @@
 
 let username;
 
-username = window.prompt("What's your name?"); //跳出視窗，使用者可輸入文字
+username = window.prompt("What's your name?"); // 跳出視窗，使用者可輸入文字
 
-console.log(username); //輸出使用者輸入的文字
+console.log(username); // 輸出使用者輸入的文字
